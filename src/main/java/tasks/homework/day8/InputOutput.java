f8daf3341b7f2b1b40cf167b483aea4a76e09ab9
Marina_Skarzhynskaya <mariettas96@mail.ru>
2022-03-09 16:01:00 +0300

    - I've finished subtask 3: count the number of punctuation marks and words in a text

--- src/main/java/tasks/homework/day8/InputOutput.java
+++ src/main/java/tasks/homework/day8/InputOutput.java
@@ -1,9 +1,14 @@
 package tasks.homework.day8;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public  class InputOutput {
 
@@ -20,4 +25,31 @@ public  class InputOutput {
 
     }
 
+    public static void countPunctuationAndWords(String file) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
+        String text = "";
+        int punctuationNumber = 0;
+        int wordsNumber = 0;
+
+        while ((line = reader.readLine()) != null) {
+            text += " " + line;
+        }
+        reader.close();
+
+        Matcher matcherPunctuation = Pattern.compile("[.,?!:;-]").matcher(text);
+        Matcher matcherWords = Pattern.compile("\\w+").matcher(text);
+
+        while (matcherPunctuation.find()) {
+            punctuationNumber++;
+        }
+
+        while (matcherWords.find()) {
+            wordsNumber++;
+        }
+
+        System.out.printf("There are %s words and %s punctuation marks in the text", wordsNumber, punctuationNumber);
+
+    }
 }
--- src/main/java/tasks/homework/day8/Runner.java
+++ src/main/java/tasks/homework/day8/Runner.java
@@ -1,12 +1,14 @@
 package tasks.homework.day8;
 
+import java.io.IOException;
 import java.text.ParseException;
 
 public class Runner {
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, IOException {
 
         InputOutput.printNextMonth();
+        InputOutput.countPunctuationAndWords("file.txt");
 
     }
 }
