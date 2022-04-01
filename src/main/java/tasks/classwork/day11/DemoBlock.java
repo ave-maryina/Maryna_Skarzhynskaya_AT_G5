e6f290bc18a4b281308a24cdd11bc9e8d0dc3465
Marina_Skarzhynskaya <mariettas96@mail.ru>
2022-04-02 02:14:12 +0300

    - Class work: Threads

---
+++ src/main/java/tasks/classwork/day11/DemoBlock.java
@@ -0,0 +1,24 @@
+package tasks.classwork.day11;
+
+public class DemoBlock {
+
+    int x;
+    static int y;
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public  int getY() {
+        return y;
+    }
+
+    public  void setY(int y) {
+        DemoBlock.y = y;
+    }
+}
